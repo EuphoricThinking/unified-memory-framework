5d7f8924c68808c52c4bde41592bcb683804bd0c
Agata Momot <agata.momot@intel.com>
2024-08-20 13:26:47 +0200

    Wrong 219; syntax check needed

--- test/memspaces/memspace_fixtures.hpp
+++ test/memspaces/memspace_fixtures.hpp
@@ -215,7 +215,8 @@ TEST_P(memspaceProviderTest, allocLocalMt) {
         ASSERT_EQ(hwloc_get_local_numanode_objs(topology, &loc, &nNodes,
                                                   localNodes.data(), 0),
                     0);
-        UT_ASSERT(nNodes <= MAX_NODES);
+        // UT_ASSERT(nNodes <= MAX_NODES);
+        ASSERT_FALSE(nNodes <= MAX_NODES);
 
         // Confirm that the allocation from this thread was made to a local
         // NUMA node.
