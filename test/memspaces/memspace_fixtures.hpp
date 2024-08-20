2cb95fafa149543d53e25c828ba6289fa49b8208
Agata Momot <agata.momot@intel.com>
2024-08-20 13:23:35 +0200

    Correct 189

--- test/memspaces/memspace_fixtures.hpp
+++ test/memspaces/memspace_fixtures.hpp
@@ -186,7 +186,7 @@ TEST_P(memspaceProviderTest, allocLocalMt) {
         umf_result_t ret =
             umfMemoryProviderAlloc(hProvider, size, alignment, &ptr);
         // UT_ASSERTeq(ret, UMF_RESULT_SUCCESS);
-        ASSERT_NE(ret, UMF_RESULT_SUCCESS);
+        ASSERT_EQ(ret, UMF_RESULT_SUCCESS);
         UT_ASSERTne(ptr, nullptr);
 
         // Access the allocation, so that all the pages associated with it are
