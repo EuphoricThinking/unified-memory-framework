3eac682d5861d35762d5d639f34d58a7e59a3861
Agata Momot <agata.momot@intel.com>
2024-09-27 16:14:21 +0200

    Copied testjs to data.js

---
+++ dev/bench/data.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+        "unit": "Percent",
+        "value": 15
+    },
+    {
+        "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+        "unit": "Megabytes",
+        "value": 25,
+        "range": "3",
+        "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+    }
+]
