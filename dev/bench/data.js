8a1fed704d7cb80fe3ce3cb683f67260c00c084b
github-action-benchmark <github@users.noreply.github.com>
2024-09-27 14:13:38 +0000

    add My Project Benchmark (customSmallerIsBetter) benchmark result for fbc266daeaf1fd03d56ede7693a4b80c5208fe61

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727443720512,
+  "lastUpdate": 1727446418521,
   "repoUrl": "https://github.com/EuphoricThinking/unified-memory-framework",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,44 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/274aebb814482e6f3bfacd30eee39a19d283f90d"
         },
         "date": 1727443719892,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      }
+    ],
+    "My Project Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "fbc266daeaf1fd03d56ede7693a4b80c5208fe61",
+          "message": "Added write and name",
+          "timestamp": "2024-09-27T16:13:19+02:00",
+          "tree_id": "cdfdbbed0781944760fa937b2e31118490c9a206",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/fbc266daeaf1fd03d56ede7693a4b80c5208fe61"
+        },
+        "date": 1727446417675,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
