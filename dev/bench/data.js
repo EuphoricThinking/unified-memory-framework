5f891a2aceb44a3de01771732a25a6fc786aa95d
github-action-benchmark <github@users.noreply.github.com>
2024-09-27 13:28:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 274aebb814482e6f3bfacd30eee39a19d283f90d

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727443720512,
+  "repoUrl": "https://github.com/EuphoricThinking/unified-memory-framework",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "274aebb814482e6f3bfacd30eee39a19d283f90d",
+          "message": "Add fragments of gh-pages example yml",
+          "timestamp": "2024-09-27T15:28:23+02:00",
+          "tree_id": "3da04d71d0ecb2e715ff0953e96451a4dc39fba8",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/274aebb814482e6f3bfacd30eee39a19d283f90d"
+        },
+        "date": 1727443719892,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      }
+    ]
+  }
+}
