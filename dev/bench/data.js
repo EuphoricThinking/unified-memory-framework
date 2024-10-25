4f377eec2932485359e298329a8fbc44da2b5994
Agata Momot <agata.momot@intel.com>
2024-10-25 03:35:39 +0200

    Branch modified to benchmark_test, copied data.js files from gh-pages

---
+++ dev/bench/data.js
@@ -0,0 +1,406 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728446416788,
+  "repoUrl": "https://github.com/EuphoricThinking/unified-memory-framework",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "274aebb814482e6f3bfacd30eee39a19d283f90d",
+          "message": "Add fragments of gh-pages example yml",
+          "timestamp": "2024-09-27T15:28:23+02:00",
+          "tree_id": "3da04d71d0ecb2e715ff0953e96451a4dc39fba8",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/274aebb814482e6f3bfacd30eee39a19d283f90d"
+        },
+        "date": 1727443719892,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      }
+    ],
+    "My Project Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "fbc266daeaf1fd03d56ede7693a4b80c5208fe61",
+          "message": "Added write and name",
+          "timestamp": "2024-09-27T16:13:19+02:00",
+          "tree_id": "cdfdbbed0781944760fa937b2e31118490c9a206",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/fbc266daeaf1fd03d56ede7693a4b80c5208fe61"
+        },
+        "date": 1727446417675,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "7d932f7d2814c107c0cb9309828530cdb63fc188",
+          "message": "Remove index.html from dev/bench",
+          "timestamp": "2024-09-27T16:53:35+02:00",
+          "tree_id": "aac72ecb11ba85126dd5a09ae66b831cbfbaf8a0",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/7d932f7d2814c107c0cb9309828530cdb63fc188"
+        },
+        "date": 1727448972351,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "7e443231688ba740be62d333ae11af21abfb4363",
+          "message": "Test create gh-pages from action",
+          "timestamp": "2024-10-04T15:18:24+02:00",
+          "tree_id": "fdc136ea442a8be620f0833c6eac10da01b2b78e",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/7e443231688ba740be62d333ae11af21abfb4363"
+        },
+        "date": 1728047937299,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "d76a37850297042f9032ccd57de37a80c37f0508",
+          "message": "Updated environment in gh settings",
+          "timestamp": "2024-10-04T15:26:42+02:00",
+          "tree_id": "c0c9a7ec9cf95db21b93161caa303b0df588fbda",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/d76a37850297042f9032ccd57de37a80c37f0508"
+        },
+        "date": 1728048415747,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "4564f428aabeb6b0aa67dc97dc8d5ae887f83575",
+          "message": "dev/bench not visible",
+          "timestamp": "2024-10-07T17:00:33+02:00",
+          "tree_id": "fdc136ea442a8be620f0833c6eac10da01b2b78e",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/4564f428aabeb6b0aa67dc97dc8d5ae887f83575"
+        },
+        "date": 1728313245291,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "fa20944f62be795caed9f2dd02c0a71af159982e",
+          "message": "Add deployment and artifact-upload to benchmark_test",
+          "timestamp": "2024-10-09T05:29:54+02:00",
+          "tree_id": "05a6f319c062e2f14988365ba60eaf0596158143",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/fa20944f62be795caed9f2dd02c0a71af159982e"
+        },
+        "date": 1728444609704,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "b612929194515f4a1d157247c9656e099e5e229d",
+          "message": "Chcekout gh-pages (benchtest)",
+          "timestamp": "2024-10-09T05:39:15+02:00",
+          "tree_id": "8ebda5e6b0d8b1046e86b752c8c351221f843204",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/b612929194515f4a1d157247c9656e099e5e229d"
+        },
+        "date": 1728445167757,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "8c84ea66c5723e2973472a2323a65165511d5e12",
+          "message": "Modify deployment branch",
+          "timestamp": "2024-10-09T05:49:24+02:00",
+          "tree_id": "c48d0618bcb26528efec1d3576e579c83f55bb0a",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/8c84ea66c5723e2973472a2323a65165511d5e12"
+        },
+        "date": 1728445773905,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "ac2541faedc8eee070f92345c3c9bf522ae5c143",
+          "message": "Modify deployment url",
+          "timestamp": "2024-10-09T05:49:40+02:00",
+          "tree_id": "c48d0618bcb26528efec1d3576e579c83f55bb0a",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/ac2541faedc8eee070f92345c3c9bf522ae5c143"
+        },
+        "date": 1728445835001,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "committer": {
+            "email": "agata.momot@intel.com",
+            "name": "Agata Momot",
+            "username": "EuphoricThinking"
+          },
+          "distinct": true,
+          "id": "baba790aced0e2527df94420e172e8b086c676a3",
+          "message": "Deploy without docs",
+          "timestamp": "2024-10-09T05:59:10+02:00",
+          "tree_id": "2eaa7b2e1c4166c5651b4825b798063ef1ed4c81",
+          "url": "https://github.com/EuphoricThinking/unified-memory-framework/commit/baba790aced0e2527df94420e172e8b086c676a3"
+        },
+        "date": 1728446416584,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      }
+    ]
+  }
+}
