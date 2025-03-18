a36556ecd6523869596911143f9821bfa22c4654
Agata Momot <agata.momot@intel.com>
2025-03-18 12:58:53 +0100

    Initial setup, hopefully nothing ommitted

---
+++ scripts/benchmarks/utils/result.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2024-2025 Intel Corporation
+# Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
+# See LICENSE.TXT
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from dataclasses import dataclass, field
+from typing import Optional
+from dataclasses_json import config, dataclass_json
+from datetime import datetime
+
+
+@dataclass_json
+@dataclass
+class Result:
+    label: str
+    value: float
+    command: list[str]
+    env: dict[str, str]
+    stdout: str
+    passed: bool = True
+    unit: str = ""
+    explicit_group: str = ""
+    # stddev can be optionally set by the benchmark,
+    # if not set, it will be calculated automatically.
+    stddev: float = 0.0
+    # values below should not be set by the benchmark
+    name: str = ""
+    lower_is_better: bool = True
+    suite: str = "Unknown"
+    description: str = "No description provided."
+
+
+@dataclass_json
+@dataclass
+class BenchmarkRun:
+    results: list[Result]
+    name: str = "This PR"
+    hostname: str = "Unknown"
+    git_hash: str = ""
+    github_repo: str = None
+    date: datetime = field(
+        default=None,
+        metadata=config(encoder=datetime.isoformat, decoder=datetime.fromisoformat),
+    )
+
+
+@dataclass_json
+@dataclass
+class BenchmarkMetadata:
+    type: str = "benchmark"  # or 'group'
+    description: Optional[str] = None
+    notes: Optional[str] = None
+    unstable: Optional[str] = None
